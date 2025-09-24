6f5449c1eb4ca356f29921aa5ca8792d8288b616
Nisha Rathore <rathorenisha397@gmail.com>
2025-09-24 23:59:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import Navbar from './components/Navbar';
 import Logo from './components/Logo';
 import Features from './components/Features';
 import Login from './pages/Login';
+import Main from './pages/Main';
 
 
 function App() {
   return (
     <>
-   <Login />
+      {/* <Navbar />
+      <Logo />
+      <Features />
+      <Login /> */}
+      <Main />
+
     </>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import image from "../image/login.jpg";
+import image from "../image/login photo.jpg";
+import Navbar from '../components/Navbar';
 
 const Login = () => {
   return (
-    <div className='mx-10 pt-10 w-full h-screen'>
-        <img src={image} alt="Background" className="absolute top-0 left-0 w-full h-full object-cover opacity-100" />
+    <>
+    <Navbar />
+    <div className='mx-10 pt-10 w-full h-screen bg-gray-300 relative '>
+        <img src={image} alt="Background" className="absolute top-0 left-0 w-full h-full object-cover" />
+        <div className='flex justify-center items-center h-full'>
+            <form className='bg-black p-10 rounded-lg shadow-md w-full max-w-md relative z-10 opacity-60'>
+                <h2 className='text-2xl font-bold mb-6 text-center text-white opacity-100'>Login to GMS</h2>
+                <div className='mb-4'>
+                    <label className='block text-gray-200 mb-2' htmlFor='username'>Username</label>
+                    <input className='w-full px-3 py-2 border border-gray-300 rounded' type='text' id='username' name='username' required />
+                </div>
+                <div className='mb-6'>
+                    <label className='block text-gray-200 mb-2' htmlFor='password'>Password</label>
+                    <input className='w-full px-3 py-2 border border-gray-300 rounded' type='password' id='password' name='password' required />
+                </div>
+                <div className='mb-6 flex items-center'>
+                    <input className='mr-2 leading-tight' type='checkbox' id='remember' name='remember' />
+                    <label className='text-gray-200' htmlFor='remember'>Remember me</label>
+                </div>
+                <div className='mb-6'>
+                    <a className='text-orange-500 hover:underline' href='#'>Forgot Password?</a>
+                    <a className='text-orange-400 hover:underline' href='#'>Create an Account</a>
+                </div>
+
+                <button className='w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300' style={{backgroundColor:"#190630ff",opacity:'100%'}} type='submit'>Login</button>
+            </form>
+        </div>
+
         
     </div>
+    </>
   )
 }
 
